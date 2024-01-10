1abaa185095c6c8a59efcc969babe46bf3315b36
Rini <Reneortega2468@gmail.com>
2024-01-10 18:01:17 -0500

    Added swipable months

---
+++ src/components/SwipyMonthYear/index.tsx
@@ -0,0 +1,102 @@
+import React, { useState, useRef } from 'react'
+import { StyleSheet, Text, View, Animated } from 'react-native'
+import { PanGestureHandler, GestureHandlerRootView } from 'react-native-gesture-handler'
+import { typography } from '../../theme/fonts'
+
+export const SwipableMonthYear: React.FC = () => {
+    const [currentDate, setCurrentDate] = useState(new Date())
+    const translateX = useRef(new Animated.Value(0)).current // For animation
+
+    const getNextMonthDate = () => {
+        let nextMonthDate = new Date(currentDate)
+        nextMonthDate.setMonth(currentDate.getMonth() + 1)
+        return nextMonthDate
+    }
+
+    const handleSwipe = (direction: string) => {
+        let newDate = new Date(currentDate)
+        if (direction === 'left') {
+            newDate.setMonth(newDate.getMonth() + 1)
+        } else if (direction === 'right') {
+            newDate.setMonth(newDate.getMonth() - 1)
+        }
+        setCurrentDate(newDate)
+        // Reset animation
+        Animated.spring(translateX, {
+            toValue: 0,
+            useNativeDriver: true
+        }).start()
+    }
+
+    const onGestureEvent = Animated.event([{ nativeEvent: { translationX: translateX } }], {
+        useNativeDriver: true
+    })
+
+    const onHandlerStateChange = (event: { nativeEvent: { velocityX: number; state: number } }) => {
+        const { velocityX, state } = event.nativeEvent
+        const swipeThreshold = 0.3 // Adjust swipe sensitivity
+
+        if (state === 5) {
+            // State 5 is 'end'
+            if (velocityX > swipeThreshold) {
+                handleSwipe('right')
+            } else if (velocityX < -swipeThreshold) {
+                handleSwipe('left')
+            } else {
+                // Reset position if not swiped enough
+                Animated.spring(translateX, {
+                    toValue: 0,
+                    useNativeDriver: true
+                }).start()
+            }
+        }
+    }
+
+    const nextMonthDate = getNextMonthDate()
+
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <PanGestureHandler
+                onGestureEvent={onGestureEvent}
+                onHandlerStateChange={onHandlerStateChange}
+            >
+                <Animated.View
+                    style={[styles.container, { transform: [{ translateX: translateX }] }]}
+                >
+                    <Text style={styles.currentDateText}>
+                        {`${currentDate.toLocaleString('default', {
+                            month: 'long'
+                        })} ${currentDate.getFullYear()}`}
+                    </Text>
+                    <Text style={styles.nextDateText}>
+                        {`${nextMonthDate.toLocaleString('default', { month: 'long' })}`}
+                    </Text>
+                </Animated.View>
+            </PanGestureHandler>
+        </GestureHandlerRootView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        width: '150%',
+        height: '10%',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingLeft: '8%'
+    },
+    currentDateText: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        fontFamily: typography.quaternary,
+        color: '#fff',
+        width: '50%'
+    },
+    nextDateText: {
+        fontSize: 30,
+        color: '#aaa',
+        fontFamily: typography.quaternary,
+        width: '50%'
+    }
+})
--- src/screens/Calender.tsx
+++ src/screens/Calender.tsx
@@ -1,19 +1,45 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { AppStackScreenProps } from '../navigators'
+import { SwipableMonthYear } from '../components/SwipyMonthYear'
+import { typography } from '../theme/fonts'
+import { weekDays } from '../constants/dates'
+import { palette } from '../theme'
 
 interface CalenderScreenProps extends AppStackScreenProps<'CalenderScreen'> {}
 
 export const CalenderScreen: React.FC<CalenderScreenProps> = () => {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}
-        >
-            <Text>Calender Screen</Text>
+        <View style={styles.container}>
+            <SwipableMonthYear />
+            <View style={styles.weekDaysView}>
+                {weekDays.map((dayName, index) => (
+                    <Text key={index} style={styles.weekDay}>
+                        {dayName}
+                    </Text>
+                ))}
+            </View>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: '15%'
+    },
+    weekDaysView: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '90%',
+        alignSelf: 'center',
+        paddingVertical: '2%',
+        backgroundColor: palette.pastelLightGreen,
+        borderRadius: 5,
+        marginBottom: '2%'
+    },
+    weekDay: {
+        width: '14.28%',
+        textAlign: 'center',
+        fontFamily: typography.primary
+    }
+})
