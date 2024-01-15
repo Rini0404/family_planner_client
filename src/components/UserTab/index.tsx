d4b59bbcf0c2004f7a66acf82ec8568d73bc5c98
Rini <Reneortega2468@gmail.com>
2024-01-15 13:21:15 -0500

    Getting initials functions. move to a util later

--- src/components/UserTab/index.tsx
+++ src/components/UserTab/index.tsx
@@ -8,6 +8,13 @@ type UserTypeProps = {
 }
 
 export const UserTab: React.FC<UserTypeProps> = ({ user }) => {
+    console.log('user', user)
+    const { firstName, lastName } = user
+    const userInitial =
+        firstName && firstName.length > 0 && lastName
+            ? firstName[0].toUpperCase() + lastName[0].toUpperCase()
+            : ''
+    console.log('userInitial', userInitial)
     return (
         <View style={styles.container}>
             <Text>Hello, {user.firstName} !</Text>
