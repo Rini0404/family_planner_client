d282ed881d2836b39cf52e161051dc2aeae5b6cb
Alan Pereyro <alanpereyro0@gmail.com>
2024-01-17 12:26:24 -0500

    fixed clipboard issue, also users properly display tasks and initials

--- src/screens/FamilyScreen.tsx
+++ src/screens/FamilyScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, StyleSheet, StatusBar, Platform, FlatList } from 'react-native'
+import { Text, View, StyleSheet, StatusBar, Platform } from 'react-native'
 import { AppStackParamList, AppStackScreenProps } from '../navigators'
 import { typography } from '../theme/fonts'
 import BackArrow from '../components/BackArrow'
@@ -8,23 +8,32 @@ import { UserType } from '../types/user'
 import { NavigationProp, useNavigation } from '@react-navigation/native'
 import { useSelector } from 'react-redux'
 import CopyIcon from '../../assets/navbar-icons/copy'
+import * as Clipboard from 'expo-clipboard'
+import { InterfaceTask } from '../types/tasks'
 
 interface FamilyScreenProps extends AppStackScreenProps<'FamilyScreen'> {}
 
 export const FamilyScreen: React.FC<FamilyScreenProps> = () => {
     const navigation = useNavigation<NavigationProp<AppStackParamList>>()
 
-    const { user } = useSelector((state: any) => state.user)
     const { family } = useSelector((state: any) => state.family)
     const { tasks } = useSelector((state: any) => state.tasks)
-
-    const userInitials =
-        user.firstName.charAt(0).toUpperCase() + user.lastName.charAt(0).toUpperCase()
 
     const handleBackPress = () => {
         navigation.goBack()
     }
 
+    const copyToClipboard = async () => {
+        await Clipboard.setStringAsync(family.invitationCode)
+    }
+
+    const getUserInitials = (firstName: string, lastName: string) => {
+        return firstName.charAt(0).toUpperCase() + lastName.charAt(0).toUpperCase()
+    }
+
+    const getTaskCountForUser = (userId: string) => {
+        return tasks.filter((task: InterfaceTask) => task.assignedTo?._id === userId).length
+    }
 
     return (
         <View style={styles.container}>
@@ -36,20 +45,27 @@ export const FamilyScreen: React.FC<FamilyScreenProps> = () => {
                 <Text style={styles.inviteText}>{family.familyName}'s Invite Code:</Text>
                 <Text style={styles.inviteText}>
                     {family.invitationCode}
-                    <CopyIcon />
+                    <CopyIcon onPress={copyToClipboard} />
                 </Text>
             </View>
             <View style={styles.familyMembersCard}>
                 <Text style={styles.familyMembersCardText}>Family Members</Text>
-                {family.members.map((member: UserType, index: number) => (
-                    <View key={index} style={styles.familyMemberList}>
-                        <Text style={styles.initials}>{userInitials}</Text>
-                        <Text style={styles.familyMemberText}>
-                            {member.firstName} {member.lastName}
-                        </Text>
-                        <Text style={styles.familyMemberText}>{tasks.length}</Text>
-                    </View>
-                ))}
+                {family.members.map((member: UserType, index: number) => {
+                    const initials = getUserInitials(member.firstName, member.lastName)
+                    const taskCount = getTaskCountForUser(member._id)
+                    const taskText = taskCount === 1 ? 'Task' : 'Tasks'
+                    return (
+                        <View key={index} style={styles.familyMemberList}>
+                            <Text style={styles.initials}>{initials}</Text>
+                            <Text style={styles.familyMemberText}>
+                                {member.firstName} {member.lastName}
+                            </Text>
+                            <Text style={styles.familyMemberText}>
+                                {taskCount} {taskText}
+                            </Text>
+                        </View>
+                    )
+                })}
             </View>
         </View>
     )
@@ -58,7 +74,8 @@ export const FamilyScreen: React.FC<FamilyScreenProps> = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+        paddingHorizontal: 10
     },
     headerText: {
         fontFamily: typography.tertiary,
@@ -80,12 +97,18 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         justifyContent: 'center',
         alignItems: 'center',
-        marginTop: '5%'
+        padding: 10,
+        marginTop: '5%',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 6,
+        elevation: 3
     },
     inviteText: {
         fontFamily: typography.primary,
         fontSize: 20,
-        textAlign: 'center',
+        textAlign: 'center'
     },
     familyMembersCard: {
         height: '50%',
@@ -93,24 +116,30 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         backgroundColor: palette.neutral100,
         borderRadius: 20,
-        marginTop: '5%'
+        padding: 10,
+        marginTop: '5%',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 6,
+        elevation: 3
     },
     familyMembersCardText: {
         fontFamily: typography.tertiary,
         fontSize: 30,
-        textAlign: 'center'
+        textAlign: 'center',
+        marginBottom: 10
     },
     familyMemberList: {
         height: '12%',
         width: '90%',
-        backgroundColor: palette.pastelNavbars,
-        borderRadius: 20,
         alignSelf: 'center',
         marginBottom: '5%',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        paddingHorizontal: '5%'
+        borderBottomWidth: 1,
+        borderBottomColor: 'black'
     },
     familyMemberText: {
         fontFamily: typography.primary,
@@ -120,13 +149,12 @@ const styles = StyleSheet.create({
     initials: {
         width: 31,
         height: 31,
-        borderRadius: 50,
+        borderRadius: 20,
         borderColor: palette.neutral800,
-        borderWidth: 1,
-        backgroundColor: palette.pastelNavbars,
+        borderWidth: 2,
         fontSize: 16,
         fontFamily: typography.quaternary,
         textAlign: 'center',
-        textAlignVertical: 'center'
+        lineHeight: 30
     }
 })
