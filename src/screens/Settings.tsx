84d97baf3e5f149741d6aea3d24908d6ad20d0bf
Alan Pereyro <alanpereyro0@gmail.com>
2024-01-30 12:43:17 -0500

    created modals for the name, password and , leaving the family button

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Platform, StyleSheet, StatusBar, Touchable } from 'react-native'
+import { View, Text, Platform, StyleSheet, StatusBar, Modal, TextInput } from 'react-native'
 import React from 'react'
 import { AppStackParamList, AppStackScreenProps } from '../navigators'
 import { palette } from '../theme'
@@ -7,6 +7,7 @@ import BackArrow from '../components/BackArrow'
 import { NavigationProp, useNavigation } from '@react-navigation/native'
 import { useSelector } from 'react-redux'
 import { TouchableOpacity } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 interface SettingsProps extends AppStackScreenProps<'Settings'> {}
 
@@ -21,49 +22,229 @@ export const Settings: React.FC<SettingsProps> = () => {
         navigation.goBack()
     }
 
+    const [isNameModalVisible, setIsNameModalVisible] = React.useState(false)
+    const [isPasswordModalVisible, setIsPasswordModalVisible] = React.useState(false)
+    const [isLeaveFamilyModalVisible, setIsLeaveFamilyModalVisible] = React.useState(false)
+    const [firstName, setFirstName] = React.useState('')
+    const [lastName, setLastName] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const [confirmPassword, setConfirmPassword] = React.useState('')
+
+    const toggleNameModal = () => {
+        setIsNameModalVisible(!isNameModalVisible)
+    }
+
+    const togglePasswordModal = () => {
+        setIsPasswordModalVisible(!isPasswordModalVisible)
+    }
+
+    const toggleLeaveFamilyModal = () => {
+        setIsLeaveFamilyModalVisible(!isLeaveFamilyModalVisible)
+    }
+
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <BackArrow onPress={handleBackPress} />
-                <Text style={styles.headerText}>Settings</Text>
+        <>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <BackArrow onPress={handleBackPress} />
+                    <Text style={styles.headerText}>Settings</Text>
+                </View>
+                <View style={styles.userInfoCard}>
+                    <Text style={styles.cardTitleText}>User Information</Text>
+                    <TouchableOpacity style={styles.row} onPress={toggleNameModal}>
+                        <Text style={styles.cardSubChangableText}>
+                            Name: {user.firstName} {user.lastName}
+                        </Text>
+                        <View style={{ marginTop: 5 }}>
+                            <Icon name='chevron-right' size={20} />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.row} onPress={togglePasswordModal}>
+                        <Text style={styles.cardSubChangableText}>Password: ********</Text>
+                        <View style={{ marginTop: 5 }}>
+                            <Icon name='chevron-right' size={20} />
+                        </View>
+                    </TouchableOpacity>
+
+                    <View style={styles.nonChangeable}>
+                        <Text style={styles.cardSubText}>Email:</Text>
+                        <Text style={styles.cardSubText}>{user.email}</Text>
+                    </View>
+                    <View style={styles.nonChangeable}>
+                        <Text style={styles.cardSubText}>Role:</Text>
+                        <Text style={styles.cardSubText}>{user.role}</Text>
+                    </View>
+                </View>
+
+                <View style={styles.familyCard}>
+                    <Text style={styles.cardTitleText}>Family Information</Text>
+                    <View style={styles.nonChangeable}>
+                        <Text style={styles.cardSubText}>Family Name:</Text>
+                        <Text style={styles.cardSubText}> {family.familyName}'s</Text>
+                    </View>
+                    <View style={styles.nonChangeable}>
+                        <Text style={styles.cardSubText}>Invite Code:</Text>
+                        <Text style={styles.cardSubText}>{family.invitationCode}</Text>
+                    </View>
+                    <View style={styles.nonChangeable}>
+                        <Text style={styles.cardSubText}>Members:</Text>
+                        <Text style={styles.cardSubText}>{family.members.length}</Text>
+                    </View>
+                    <TouchableOpacity style={styles.button} onPress={toggleLeaveFamilyModal}>
+                        <Text style={styles.buttonText}>Leave this Family</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View style={styles.userInfoCard}>
-                <Text style={styles.cardText}>
-                    {user.firstName} {user.lastName}
-                </Text>
-                <Text style={styles.cardSubText}>Email: {user.email}</Text>
-                <Text style={styles.cardSubText}>Role: {user.role}</Text>
-                <TouchableOpacity style={styles.button} onPress={() => {}} activeOpacity={0.7}>
-                    <Text style={styles.buttonText}>Edit</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={styles.familyCard}>
-                <Text style={styles.cardText}>Family: {family.familyName}'s</Text>
-                <Text style={styles.cardSubText}>Invite Code: {family.invitationCode}</Text>
-                <TouchableOpacity style={styles.button} onPress={() => {}} activeOpacity={0.7}>
-                    <Text style={styles.buttonText}>Leave this Family</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={styles.passwordCard}>
-                <Text style={styles.cardText}>Password: ********</Text>
-                <TouchableOpacity style={styles.button} onPress={() => {}} activeOpacity={0.7}>
-                    <Text style={styles.buttonText}>Change Password</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={styles.reportCard}>
-                <Text style={styles.cardText}>Report a Problem</Text>
-                <TouchableOpacity style={styles.button} onPress={() => {}} activeOpacity={0.7}>
-                    <Text style={styles.buttonText}>Report</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
+            {isNameModalVisible && (
+                <Modal
+                    animationType='slide'
+                    transparent={true}
+                    visible={isNameModalVisible}
+                    onRequestClose={toggleNameModal}
+                >
+                    <View style={styles.modalOverlay}>
+                        <View style={styles.modalView}>
+                            <View style={styles.modalHeader}>
+                                <TouchableOpacity onPress={toggleNameModal}>
+                                    <Text style={styles.modalHeaderText}>Cancel</Text>
+                                </TouchableOpacity>
+                                <Text style={styles.modalHeaderTitle}>Edit Info</Text>
+                                <TouchableOpacity onPress={() => {}}>
+                                    <Text style={styles.modalHeaderText}>Save</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={styles.modalContent}>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder={user.firstName}
+                                    value={firstName}
+                                    onChangeText={(text) => setFirstName(text)}
+                                />
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder={user.lastName}
+                                    value={lastName}
+                                    onChangeText={(text) => setLastName(text)}
+                                />
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
+            )}
+            {isPasswordModalVisible && (
+                <Modal
+                    animationType='slide'
+                    transparent={true}
+                    visible={isPasswordModalVisible}
+                    onRequestClose={togglePasswordModal}
+                >
+                    <View style={styles.modalOverlay}>
+                        <View style={styles.modalView}>
+                            <View style={styles.modalHeader}>
+                                <TouchableOpacity onPress={togglePasswordModal}>
+                                    <Text style={styles.modalHeaderText}>Cancel</Text>
+                                </TouchableOpacity>
+                                <Text style={styles.modalHeaderTitle}>Edit Info</Text>
+                                <TouchableOpacity onPress={() => {}}>
+                                    <Text style={styles.modalHeaderText}>Save</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={styles.modalContent}>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder='Password'
+                                    value={password}
+                                    onChangeText={(text) => setPassword(text)}
+                                />
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder='Confirm Password'
+                                    value={confirmPassword}
+                                    onChangeText={(text) => setConfirmPassword(text)}
+                                />
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
+            )}
+            {isLeaveFamilyModalVisible && (
+                <Modal
+                    animationType='slide'
+                    transparent={true}
+                    visible={isLeaveFamilyModalVisible}
+                    onRequestClose={toggleLeaveFamilyModal}
+                >
+                    <View style={styles.modalOverlay}>
+                        <View style={styles.modalView}>
+                            <View style={styles.modalHeader}>
+                                <TouchableOpacity onPress={toggleLeaveFamilyModal}>
+                                    <Text style={styles.modalHeaderText}>Cancel</Text>
+                                </TouchableOpacity>
+                                <Text style={styles.modalHeaderTitle}>Leave Family</Text>
+                                <TouchableOpacity onPress={() => {}}>
+                                    <Text style={styles.modalHeaderText}>Leave</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={styles.modalContent}>
+                                <Text style={{ fontFamily: typography.primary, fontSize: 16 }}>
+                                    Are you sure you want to leave this family?
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
+            )}
+        </>
     )
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : '10%',
         paddingHorizontal: 10
+    },
+    modalOverlay: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    modalView: {
+        width: '95%',
+        height: '90%',
+        backgroundColor: palette.neutral100,
+        borderRadius: 10,
+        padding: 20,
+        alignItems: 'center'
+    },
+    modalHeader: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 20
+    },
+    modalHeaderTitle: {
+        fontSize: 18,
+        fontFamily: typography.tertiary,
+        textAlign: 'center'
+    },
+    modalHeaderText: {
+        fontSize: 16,
+        color: 'blue',
+        fontFamily: typography.tertiary
+    },
+    modalContent: {
+        width: '100%'
+    },
+    input: {
+        width: '100%',
+        padding: 10,
+        marginVertical: 10,
+        borderWidth: 1,
+        borderColor: palette.neutral700,
+        borderRadius: 5,
+        fontFamily: typography.primary
     },
     headerText: {
         fontFamily: typography.tertiary,
@@ -77,80 +258,64 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center'
     },
+    nonChangeable: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 5,
+        width: '95%',
+        alignSelf: 'center'
+    },
     userInfoCard: {
-        height: '20%',
+        height: '25%',
         width: '95%',
         alignSelf: 'center',
         backgroundColor: palette.neutral100,
-        borderRadius: 20,
-        padding: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 6,
-        elevation: 3
+        borderRadius: 20
     },
-    familyCard: {
-        height: '19%',
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         width: '95%',
         alignSelf: 'center',
-        backgroundColor: palette.neutral100,
-        borderRadius: 20,
-        padding: 10,
-        marginTop: '5%',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 6,
-        elevation: 3
+        padding: 5
     },
-    passwordCard: {
-        height: '19%',
-        width: '95%',
-        alignSelf: 'center',
-        backgroundColor: palette.neutral100,
-        borderRadius: 20,
-        padding: 10,
-        marginTop: '5%',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 6,
-        elevation: 3
-    },
-    reportCard: {
-        height: '19%',
-        width: '95%',
-        alignSelf: 'center',
-        backgroundColor: palette.neutral100,
-        borderRadius: 20,
-        padding: 10,
-        marginTop: '5%',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 6,
-        elevation: 3
-    },
-    cardText: {
+    cardTitleText: {
         fontSize: 18,
-        fontFamily: typography.tertiary,
+        fontFamily: typography.quaternary,
         color: palette.neutral800,
-        marginBottom: 5
+        marginBottom: 2,
+        borderBottomColor: palette.neutral700,
+        borderBottomWidth: 1,
+        padding: 5,
+        textAlign: 'center'
     },
     cardSubText: {
         fontSize: 16,
         fontFamily: typography.primary,
-        color: palette.neutral300
+        color: palette.neutral800,
+        justifyContent: 'space-between'
+    },
+    cardSubChangableText: {
+        fontSize: 16,
+        fontFamily: typography.primary,
+        color: palette.neutral800
+    },
+    familyCard: {
+        height: '28%',
+        width: '95%',
+        alignSelf: 'center',
+        backgroundColor: palette.neutral100,
+        borderRadius: 20,
+        marginTop: 10
     },
     button: {
-        backgroundColor: palette.pastelNavbars,
-        paddingVertical: 10,
-        paddingHorizontal: 20,
+        backgroundColor: palette.angry500,
+        padding: 10,
         borderRadius: 20,
         alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 10
+        marginTop: 18,
+        width: '95%',
+        alignSelf: 'center'
     },
     buttonText: {
         color: palette.neutral100,
